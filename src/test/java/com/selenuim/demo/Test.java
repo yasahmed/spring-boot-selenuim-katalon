504314735333544df94395c5c9ab561e78ca1c0b
yasahmed <eessaouira@gmail.com>
2019-03-15 09:55:46 +0100

    first commit

---
+++ src/test/java/com/selenuim/demo/Test.java
@@ -0,0 +1,58 @@
+package com.selenuim.demo;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class Test {
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\eessa\\Desktop\\katalan\\geckodriver.exe");
+        FirefoxOptions firefoxOptions =  new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+        driver = new FirefoxDriver(firefoxOptions);
+        baseUrl = "https://www.katalon.com/";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @org.testng.annotations.Test
+    public void testUntitledTestCase() throws Exception {
+        driver.get("https://www.rainworx.com/AWDemo31/Account/LogOn?returnUrl=%2FAWDemo31");
+        driver.findElement(By.id("username")).click();
+        driver.findElement(By.id("username")).clear();
+        driver.findElement(By.id("username")).sendKeys("Admin");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("demo");
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::input[4]")).click();
+        driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Categories'])[1]/following::a[2]")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
