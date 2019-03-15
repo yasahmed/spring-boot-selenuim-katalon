9abf34ad3e33165110329028f45d832b037519b3
yasahmed <eessaouira@gmail.com>
2019-03-15 11:16:30 +0000

    Update Test.java

--- src/test/java/com/selenuim/demo/Test.java
+++ src/test/java/com/selenuim/demo/Test.java
@@ -32,6 +32,7 @@ public class Test {
     @BeforeClass(alwaysRun = true)
     public void setUp() throws Exception {
         System.setProperty("webdriver.gecko.driver", "/awb/var/lib/jenkins/workspace/EBK_TESTNg/test2/drivers/geckodriver");
+        System.setProperty("webdriver.firefox.bin", "/awb/var/lib/jenkins/workspace/EBK_TESTNg/test2/drivers/geckodrivere");
         FirefoxOptions firefoxOptions =  new FirefoxOptions();
         firefoxOptions.addArguments("--headless");
         driver = new FirefoxDriver(firefoxOptions);
